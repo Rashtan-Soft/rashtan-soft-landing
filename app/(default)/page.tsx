586d72e7093b7c9d59a366f4e4d2a54dd6fff39c
Gavrilo Mumovic <gajo357@gmail.com>
2024-04-04 11:05:35 +0200

    Delete newsletter

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -6,7 +6,6 @@ export const metadata = {
 
 import Hero from "@/components/hero";
 import Features from "@/components/features";
-import Newsletter from "@/components/newsletter";
 import Zigzag from "@/components/zigzag";
 import Testimonials from "@/components/testimonials";
 
@@ -17,7 +16,6 @@ export default function Home() {
       <Features />
       <Zigzag />
       <Testimonials />
-      // <Newsletter />
     </>
   );
 }
